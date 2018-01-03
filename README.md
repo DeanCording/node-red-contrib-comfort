8b7ae96655cec23567a2d65d1738e763bdbd91ca
Dean Cording <dean@cording.id.au>
2018-01-04 00:17:51 +1000

    Added MRT and Sensation

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ The main factors that influence thermal comfort are those that determine heat ga
 
 This node takes as a minimum the current temperature and humidity readings, and calculates the thermal comfort level for a person in that space wearing a specified amount of clothing and undertaking the specified level of activity.  An optional air speed can be included to account for environments with forced or natural air circulation (drafts). The amount of clothing and level of activity can be adjusted at runtime to account for changes of use at different times of the day ie. sleeping, working, relaxing, exercising.
 
-The calculated thermal comfort level predicts the mean value of the thermal sensation votes (self-reported perceptions) of a large group of persons on a sensation scale expressed from -3 to +3 corresponding to the categories "cold," "cool," "slightly cool," "neutral," "slight warm," "warm," and "hot."  Based on the calculated thermal comfort level, heating or cooling systems could be controlled to produce the most comfortable environmental conditions. Alternatively, you could just use the output of the node to tell you if you need to wear a jumper or not.
+Comfort level can be calculate either using both the dry bulb temperature and mean radiant temperature, or
+with just the operative temperature. As both dry bulb and radiant mean temperatures require special
+instruments to measure, and the typical electronic temperature sensor measures close to operative temperature,
+most systems will want to use operative temperature.
+
+The calculated thermal comfort level predicts the mean value of the thermal sensation votes (self-reported perceptions) of a large group of persons on a sensation scale expressed from -3 to +3 corresponding to the categories "cold", "cool", "slightly cool", "neutral", "slight warm", "warm", and "hot".  Based on the calculated thermal comfort level, heating or cooling systems could be controlled to produce the most comfortable environmental conditions. Alternatively, you could just use the output of the node to tell you if you need to wear a jumper or not.
 
 
 
--- comfort.js
+++ comfort.js
@@ -29,6 +29,8 @@ module.exports = function(RED) {
         node.name = n.name;
         node.tempField = n.tempField || "payload.temperature";
         node.tempFieldType = n.tempFieldType || "msg";
+        node.mrtField = n.mrtField || node.tempField;
+        node.mrtFieldType = n.mrtFieldType || 'operative';
         node.humidityField = n.humidityField || "payload.humidity";
         node.humidityFieldType = n.humidityFieldType || "msg";
         node.airspeedField = n.airspeedField || 0;
@@ -39,9 +41,17 @@ module.exports = function(RED) {
         node.clothingLevelFieldType = n.clothingLevelFieldType || "num";
         node.comfortField = n.comfortField  || "payload.comfort";
         node.comfortFieldType = n.comfortFieldType || "msg";
+        node.sensationField = n.sensationField  || "payload.sensation";
+        node.sensationFieldType = n.sensationFieldType || "msg";
         node.on("input", function(msg) {
 
             var temp = RED.util.evaluateNodeProperty(node.tempField,node.tempFieldType,node,msg) * 1.0;
+            var mrt;
+            if (node.mrtFieldType == 'operative') {
+                mrt = temp;
+            } else {
+                mrt = RED.util.evaluateNodeProperty(node.mrtField,node.mrtFieldType,node,msg) * 1.0;
+            }
             var humidity = RED.util.evaluateNodeProperty(node.humidityField,node.humidityFieldType,node,msg) * 1.0;
             var airspeed = RED.util.evaluateNodeProperty(node.airspeedField,node.airspeedFieldType,node,msg) * 1.0;
             var metabolicRate = RED.util.evaluateNodeProperty(node.metabolicRateField,node.metabolicRateFieldType,node,msg) * 1.0;
@@ -50,10 +60,19 @@ module.exports = function(RED) {
             var comfort;
 
             if (node.airspeed <= 0.2) {
-                comfort = comf.pmv(temp, temp, airspeed, humidity, metabolicRate, clothingLevel, 0)
+                comfort = comf.pmv(temp, mrt, airspeed, humidity, metabolicRate, clothingLevel, 0);
             } else {
-                comfort = comf.pmvElevatedAirspeed(temp, temp, airspeed, humidity, metabolicRate, clothingLevel, 0)
+                comfort = comf.pmvElevatedAirspeed(temp, mrt, airspeed, humidity, metabolicRate, clothingLevel, 0);
             }
+
+            var sensation;
+            if (comfort.pmv < -2.5) { sensation = 'cold' }
+            else if (comfort.pmv < -1.5) { sensation = 'cool' }
+            else if (comfort.pmv < -0.5) { sensation = 'clightly cool' }
+            else if (comfort.pmv < 0.5) { sensation = 'neutral' }
+            else if (comfort.pmv < 1.5) { sensation = 'slightly warm' }
+            else if (comfort.pmv < 2.5) { sensation = 'warm' }
+            else {sensation = 'hot'};
 
             if (node.comfortFieldType === 'msg') {
                 RED.util.setMessageProperty(msg,node.comfortField,comfort.pmv);
@@ -61,6 +80,14 @@ module.exports = function(RED) {
                 node.context().flow.set(node.comfortField,comfort.pmv);
             } else if (node.comfortFieldType === 'global') {
                 node.context().global.set(node.comfortField,comfort.pmv);
+            }
+
+            if (node.sensationFieldType === 'msg') {
+                RED.util.setMessageProperty(msg,node.sensationField,sensation);
+            } else if (node.sensationFieldType === 'flow') {
+                node.context().flow.set(node.sensationField,sensation);
+            } else if (node.sensationFieldType === 'global') {
+                node.context().global.set(node.sensationField,sensation);
             }
 
             node.send(msg);
